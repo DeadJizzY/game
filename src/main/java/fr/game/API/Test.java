58635ea6fdb09fb02174e8335936dc42dcd48456
Alain Godin <Lapin.xbox@hotmail.com>
2016-04-03 12:37:33 -0400

    Test

---
+++ src/main/java/fr/game/API/Test.java
@@ -0,0 +1,14 @@
+package fr.game.API;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Alain on 2016-04-03.
+ */
+public class Test extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
